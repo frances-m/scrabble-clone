597b5a95344c2bc89a77d83276d0a477cfce1b37
Frances <fr.ncesm@gmail.com>
2025-12-09 18:11:24 -0700

    refactor pending tiles logic

--- board_state.gd
+++ board_state.gd
@@ -12,20 +12,16 @@ func get_tile_at(row: int, col: int) -> Sprite2D:
 	return board[row][col]
 
 func place_tile(tile: Sprite2D, square: Node2D) -> void:
-	pending_tiles.append({
-		"tile": tile,
-		"row": square.row,
-		"col": square.col,
-	})
 	tile.square = square
 	square.tile = tile
+	pending_tiles.append(tile)
 	tile.connect("started_moving", Callable(self, "_remove_from_pending"))
 
 func _remove_from_pending(tile: Sprite2D) -> void:
 	tile.disconnect("started_moving", Callable(self, "_remove_from_pending"))
 	tile.square.tile = null
 	tile.square = null
-	var idx = pending_tiles.find_custom(func(t): return t["tile"] == tile)
+	var idx = pending_tiles.find(tile)
 	if idx == -1:
 		return
 	pending_tiles.pop_at(idx)
@@ -37,11 +33,11 @@ func score_pending() -> void:
 		return
 	
 	for tile in pending_tiles:
-		var row = tile["row"]
-		var col = tile["col"]
-		board[row][col] = tile["tile"]
-		tile["tile"].disconnect("started_moving", Callable(self, "_remove_from_pending"))
-		tile["tile"].scored = true
+		var row = tile.square.row
+		var col = tile.square.col
+		board[row][col] = tile
+		tile.disconnect("started_moving", Callable(self, "_remove_from_pending"))
+		tile.scored = true
 	
 	pending_tiles = []
 	emit_signal("tiles_scored")
--- scorer.gd
+++ scorer.gd
@@ -13,21 +13,23 @@ signal score_updated()
 
 func score(pending_tiles: Array) -> bool:
 	if pending_tiles.size() < 1:
+		print("NO TILES")
 		return false
 	
-	var compare_row = pending_tiles[0]["row"]
-	var compare_col = pending_tiles[0]["col"]
+	var compare_row = pending_tiles[0].square.row
+	var compare_col = pending_tiles[0].square.col
 	
 	# TODO: we will get the wrong shared row if there's
 	# only one tile placed above or below existing tiles
-	var share_row = pending_tiles.all(func(tile): return tile["row"] == compare_row)
-	var share_col = pending_tiles.all(func(tile): return tile["col"] == compare_col)
+	var share_row = pending_tiles.all(func(tile): return tile.square.row == compare_row)
+	var share_col = pending_tiles.all(func(tile): return tile.square.col == compare_col)
 	
 	if !share_row && !share_col:
+		print("NO SHARED ROW OR COL")
 		return false
 	
 	var axis = "row" if share_row else "col"
-	pending_tiles.sort_custom(func(a, b): return a[axis] > b[axis])
+	pending_tiles.sort_custom(func(a, b): return a.square[axis] > b.square[axis])
 	
 	var letters = []
 	var has_gap = false
@@ -39,27 +41,24 @@ func score(pending_tiles: Array) -> bool:
 	var pos: int = 0
 	
 	var first_tile = pending_tiles[0]
-	print(range(first_tile[pos_axis] - 1, -1, -1))
-	for i in range(first_tile[pos_axis] - 1, -1, -1):
-		var x = i if pos_axis == "row" else first_tile["row"]
-		var y = i if pos_axis == "col" else first_tile["col"]
-		print("existing tile: i ", i, " x ", x, " y ", y)
+	var first_tile_pos = first_tile.square[pos_axis]
+	for i in range(first_tile_pos - 1, -1, -1):
+		var x = i if pos_axis == "row" else first_tile.square.row
+		var y = i if pos_axis == "col" else first_tile.square.col
 		var existing_tile = BoardState.get_tile_at(x, y)
 		if !existing_tile:
 			break
 		touches_existing_tile = true
-		print(existing_tile.get_tile_letter())
 		letters.append(existing_tile.get_tile_letter())
 		score_tally += existing_tile.get_value()
 	
 	
-	for t in pending_tiles:
-		var tile = t["tile"]
-		
-		if pos && t[pos_axis] != pos + 1:
-			for i in range(pos + 1, t[pos_axis]):
-				var x = i if pos_axis == "row" else t["row"]
-				var y = i if pos_axis == "col" else t["col"]
+	for tile in pending_tiles:
+		var tile_pos = tile.square[pos_axis]
+		if pos && tile_pos != pos + 1:
+			for i in range(pos + 1, tile_pos):
+				var x = i if pos_axis == "row" else tile.square.row
+				var y = i if pos_axis == "col" else tile.square.col
 				var scored_tile = BoardState.get_tile_at(x, y)
 				if !scored_tile:
 					has_gap = true
@@ -68,7 +67,7 @@ func score(pending_tiles: Array) -> bool:
 					letters.append(scored_tile.get_tile_letter())
 					score_tally += scored_tile.get_value()
 		
-		pos = t[pos_axis]
+		pos = tile_pos
 		
 		# flag if on start square
 		if tile.square.type == "SS":
@@ -84,9 +83,10 @@ func score(pending_tiles: Array) -> bool:
 		score_tally += tile_value
 	
 	var last_tile = pending_tiles[-1]
-	for i in range(last_tile[pos_axis] + 1, 15, 1):
-		var x = i if pos_axis == "row" else last_tile["row"]
-		var y = i if pos_axis == "col" else last_tile["col"]
+	var last_tile_pos = last_tile.square[pos_axis]
+	for i in range(last_tile_pos + 1, 15, 1):
+		var x = i if pos_axis == "row" else last_tile.square.row
+		var y = i if pos_axis == "col" else last_tile.square.col
 		var existing_tile = BoardState.get_tile_at(x, y)
 		if !existing_tile:
 			break
@@ -97,17 +97,21 @@ func score(pending_tiles: Array) -> bool:
 	score_tally *= word_multiplier
 	
 	if first_move && !on_starting_square:
+		print("FIRST MOVE NEEDS TO BE ON STARTING SQUARE")
 		return false
 	
 	if !first_move && !touches_existing_tile:
+		print("NOT ADJACENT TO ANY EXISTING TILES")
 		return false
 	
 	if has_gap:
+		print("GAP BETWEEN TILES")
 		return false
 	
 	var word = "".join(letters)
 	print(word)
 	if !is_valid_word(word):
+		print("INVALID WORD")
 		return false
 	
 	var player = Globals.current_player
