86d014662534085f0b1f8bc1c15fbebe1ad81551
Frances <fr.ncesm@gmail.com>
2025-12-13 15:16:52 -0700

    highlight word list of current player

--- globals.gd
+++ globals.gd
@@ -1,5 +1,7 @@
 extends Node
 
+signal change_player()
+
 var current_player: int = 1
 
 var tile_size: float = 54.0
@@ -8,3 +10,4 @@ var selected_tile
 
 func switch_player() -> void:
 	current_player = 2 if current_player == 1 else 1
+	emit_signal("change_player")
--- word_list.gd
+++ word_list.gd
@@ -3,13 +3,22 @@ extends Control
 func _ready() -> void:
 	%Player.text = get_player_name()
 	%Score.set_meta("player", get_player())
+	_set_color()
 	Scorer.connect("word_played", Callable(self, "_add_word"))
+	Globals.connect("change_player", Callable(self, "_on_player_change"))
 
 func get_player() -> int:
 	return get_meta("player", 0)
 
 func get_player_name() -> String:
 	return "Player {num}".format({ "num": get_player() })
+
+func _set_color() -> void:
+	var color = Color(0, 0, 0, .8)
+	if get_player() != Globals.current_player:
+		color.a = .6
+
+	%ColorRect.color = color
 
 func _add_word(word: String, score: int) -> void:
 	if get_player() != Globals.current_player:
@@ -27,3 +36,6 @@ func _add_word(word: String, score: int) -> void:
 	score_label.label_settings = settings
 	score_label.text = str(score)
 	%Scores.add_child(score_label)
+
+func _on_player_change() -> void:
+	_set_color()
